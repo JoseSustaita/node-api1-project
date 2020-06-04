e2c8a6cc7906f109379eaf155355754b9125e44a
Jose Sustaita <josesustaita70@gmail.com>
2020-06-03 21:20:29 -0400

    Added Bio to users

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 let users = [
-  { id: "1", name: "Jane Doe" },
-  { id: "2", name: "John Doe" },
-  { id: "3", name: "Jack Doe" },
+  { id: "1", name: "Jane Doe", bio: "Not Tarzan's Wife, another Jane" },
+  { id: "2", name: "John Doe", bio: "Not Tarzan's Wife, another Jane" },
+  { id: "3", name: "Jack Doe", bio: "Not Tarzan's Wife, another Jane" },
 ];
 
 function getUsers() {
